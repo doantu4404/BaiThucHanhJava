ca3f213e886919ecb10a614f3ada7ef7c518efbe
doantu4404 <doantu4404@gmail.com>
2022-04-30 21:03:09 +0700

    Create_Project_BaiThucHanh2904

---
+++ BaiThucHanh2904/src/Triangle.java
@@ -0,0 +1,9 @@
+import shapes.triangle;
+
+public class Triagle {
+    public static void main(String[] args) {
+        triangle tri = new triangle();
+        tri.nhap3Point();
+        tri.soSanhTamGiac();
+    }
+}
